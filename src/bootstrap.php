119678843caf645b00bb7ac4d9c26544e6b7507c
evartanyan1989 <evartanyan1989@gmail.com>
2025-12-15 16:09:58 +0800

    Настроил DI

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,15 @@
 <?php
 declare(strict_types=1);
 
+use App\Container;
+use App\Services\B24Service;
+use Bitrix24\SDK\Services\ServiceBuilder;
+use Bitrix24\SDK\Services\ServiceBuilderFactory;
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
+
+$container = new Container();
+$container->set(B24Service::class,     fn() => new B24Service($container->get(ServiceBuilder::class)));
+$container->set(ServiceBuilder::class, fn() => ServiceBuilderFactory::createServiceBuilderFromWebhook($_ENV['B24_WEBHOOK_CODE']));
